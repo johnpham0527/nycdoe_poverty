c5bc4de5a1879f9c7b2815a81321e582cd113d93
John Pham <47116221+johnpham0527@users.noreply.github.com>
2019-03-07 11:54:53 -0500

    Add files via upload

--- NYCDOEPovertybyZIPCode.py
+++ NYCDOEPovertybyZIPCode.py
@@ -42,20 +42,44 @@ def getNumSchoolsByZIPCode(zipCode):
 
 def getPovertyPercentByZIPCode(zipCode, year="2017-18"):
     print("Retrieving school poverty data...")
-    schoolsArray = getSchoolsByZIPCode(zipCode)
-    povertyPercentArray = []
-    for school in schoolsArray:
-        schoolDBN = school["ats_system_code"]
+    schoolsArray = getSchoolsByZIPCode(zipCode) #generate a list of schools in the given ZIP code
+    enrollmentArray = []
+    povertyCountArray = []
+
+    for school in schoolsArray: #loop through the list of schools 
+        schoolDBN = school["ats_system_code"] #obtain each school's DBN
         modifiedSchoolDBN = schoolDBN[:-6] #remove last 6 empty white space characters
-        povertyPercent = getPovertyPercentBySchoolDBN(modifiedSchoolDBN)
-        print(modifiedSchoolDBN + ": " + povertyPercent + "%")
-        povertyPercentArray.append(povertyPercent)
-    #print(povertyPercentArray)
-    sum = 0
-    for i in povertyPercentArray:
-        sum += float(i)
-    ZIPCodePoverty = sum / len(povertyPercentArray)
-    return ZIPCodePoverty
+
+        getUrl = "https://data.cityofnewyork.us/resource/s52a-8aq6.json"
+        parameters = {"dbn":modifiedSchoolDBN, "year":year, "$$app_token":"QoQet97KEDYpMW4x4Manaflkp"} 
+        requestResult = requests.get(getUrl, params=parameters) #submit the GET request
+        resultText = requestResult.text #obtain the requested text
+        jsonText = json.loads(resultText)
+
+        povertyPercent = jsonText[0]["poverty_2"]
+
+        enrollment = jsonText[0]["total_enrollment"] #obtain the school's total enrollment
+        enrollmentArray.append(enrollment)
+        povertyCount = jsonText[0]["poverty_1"] #obtain the number of students in the school 
+        povertyCountArray.append(povertyCount)
+        
+        print("School DBN: " + modifiedSchoolDBN + "\tPoverty Percentage: " + povertyPercent + "%" + "\tPoverty Count: " + povertyCount + "\tTotal Enrollment: " + enrollment)
+
+    enrollmentSum = 0
+    for i in enrollmentArray:
+        enrollmentSum += float(i)
+
+    povertySum = 0
+    for j in povertyCountArray:
+        povertySum += float(j)
+
+    print("Total Number of Students with Poverty Status: " + str(povertySum))
+    print("Total Number of Students Enrolled in ZIP Code: " + str(enrollmentSum))
+
+    ZIPCodePovertyPercentage = povertySum / enrollmentSum * 100 #calculate the percentage of students who receive free or reduced lunch or qualify for HRA public benefits
+    return ZIPCodePovertyPercentage
+
+
 
 #schoolDBN = "01M015" #01M015 is P.S. 15 Roberto Clemente
 #print(schoolDBN + ": " + getPovertyPercentBySchoolDBN("01M015"))
@@ -66,5 +90,6 @@ zipCode = "11432"
 #print(getSchoolsByZIPCode(zipCode))
 #printSchoolsPovertyPercentbyZIPCode(zipCode)
 
+#Print the ZIP code's average NYC DOE poverty percentage.
 print("There are " + str(getNumSchoolsByZIPCode(zipCode)) + " schools in ZIP Code " + str(zipCode) + ". ")
-print("This ZIP Code's average NYC DOE poverty rate is " + str(getPovertyPercentByZIPCode(zipCode)) + "%.")
+print("This ZIP Code's average NYC DOE poverty percentage is " + str(round(getPovertyPercentByZIPCode(zipCode),1)) + "%.")
